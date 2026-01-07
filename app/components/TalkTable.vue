f2f52fce46c5179bf83514271407cea566afd965
George Roe <george.roe@stfc.ac.uk>
2026-01-07 14:14:46 +0000

    feat: turn talk table rows into links to the respective talk

--- app/components/TalkTable.vue
+++ app/components/TalkTable.vue
@@ -4,8 +4,15 @@ defineProps<{
     title: string
     speaker: string
     date: Date
+    path: string
   }[]
 }>()
+
+const router = useRouter()
+
+function goToTalk(talk: { title: string; speaker: string; date: Date; path: string }) {
+  router.push(talk.path)
+}
 </script>
 
 <template>
@@ -16,7 +23,7 @@ defineProps<{
         <th>Speaker</th>
         <th>Date</th>
       </tr>
-      <tr v-for="talk in talks" :key="talk.title">
+      <tr v-for="talk in talks" :key="talk.title" @click="goToTalk(talk)" class="cursor-pointer hover:underline">
         <td>{{ talk.title }}</td>
         <td>{{ talk.speaker }}</td>
         <td>{{ talk.date.toLocaleDateString() }}</td>

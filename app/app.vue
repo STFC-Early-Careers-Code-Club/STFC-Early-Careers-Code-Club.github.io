318704f346d78f51f080b9dcfde81dd33e28dcb2
George Roe <george.roe@stfc.ac.uk>
2026-01-07 21:30:22 +0000

    feat: add STFC colours and branding

--- app/app.vue
+++ app/app.vue
@@ -2,7 +2,7 @@
   <Body class="bg-white text-black dark:bg-neutral-900 dark:text-white">
     <NuxtRouteAnnouncer />
 
-    <main class="prose prose-neutral dark:prose-invert container mx-auto">
+    <main class="prose prose-blue dark:prose-invert container mx-auto p-4">
       <NuxtPage />
     </main>
   </Body>

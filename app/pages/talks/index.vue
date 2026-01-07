a3617cff05c86e5bd1504616fe261794bebc1552
George Roe <george.roe@stfc.ac.uk>
2026-01-07 14:46:54 +0000

    chore: swap given and upcoming talks

--- app/pages/talks/index.vue
+++ app/pages/talks/index.vue
@@ -6,14 +6,14 @@ const { givenTalks, upcomingTalks } = await useTalks()
   <div class="w-full">
     <h1>Talks</h1>
 
-    <template v-if="givenTalks && givenTalks.length">
-      <h2>Given Talks</h2>
-      <TalkTable v-if="givenTalks" :talks="givenTalks" />
-    </template>
-
     <template v-if="upcomingTalks && upcomingTalks.length">
       <h2>Upcoming Talks</h2>
       <TalkTable v-if="upcomingTalks" :talks="upcomingTalks" />
+    </template>
+
+    <template v-if="givenTalks && givenTalks.length">
+      <h2>Given Talks</h2>
+      <TalkTable v-if="givenTalks" :talks="givenTalks" />
     </template>
   </div>
 </template>

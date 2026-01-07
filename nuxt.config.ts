bd2dde8d38087c07e24dc00b2100f0c6147f1a22
George Roe <george.roe@stfc.ac.uk>
2026-01-07 15:31:11 +0000

    feat: remove extra config due to organisation change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,4 @@ export default defineNuxtConfig({
   modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
-  $production: {
-    app: {
-      baseURL: '/stfc-early-careers-code-club/'
-    },
-    nitro: {
-      preset: 'github-pages'
-    }
-  }
 })

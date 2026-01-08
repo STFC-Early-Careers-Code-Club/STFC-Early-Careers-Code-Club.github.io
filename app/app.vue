6b9cfe51993e67060684b30f7ad46ec701bb0916
George Roe <george.roe@stfc.ac.uk>
2026-01-08 09:08:34 +0000

    feat: add simple navbar

--- app/app.vue
+++ app/app.vue
@@ -2,6 +2,7 @@
   <Body class="bg-white text-black dark:bg-neutral-900 dark:text-white">
     <NuxtRouteAnnouncer />
 
+    <Navbar />
     <main class="prose prose-blue dark:prose-invert container mx-auto p-4">
       <NuxtPage />
     </main>
---
+++ app/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav class="bg-blue-500 text-white h-12 sticky top-0 z-50 flex justify-between items-center">
+    <NuxtLink to="/" class="flex items-center h-full">
+      <img src="/little-willow.png" alt="Logo" class="h-full">
+      <span class="ml-2 font-bold text-lg">STFC Early Careers Code Club</span>
+    </NuxtLink>
+
+    <NuxtLink to="https://github.com/STFC-Early-Careers-Code-Club/STFC-Early-Careers-Code-Club.github.io">
+      <IconGithub class="h-8 w-8 fill-white mr-4 my-auto" />
+    </NuxtLink>
+  </nav>
+</template>

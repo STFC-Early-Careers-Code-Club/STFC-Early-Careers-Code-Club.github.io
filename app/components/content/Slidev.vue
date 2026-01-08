1615448411e2967497851b6ab3af08ae281a47bb
George Roe <george.roe@stfc.ac.uk>
2026-01-08 16:00:01 +0000

    feat: add slidev component

---
+++ app/components/content/Slidev.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  url: string
+}>()
+</script>
+
+<template>
+  <div>
+    <iframe
+      :src="url"
+      class="w-full aspect-video border-0"
+      allowfullscreen
+    ></iframe>
+    <NuxtLink :to="url">Open Presentation in new tab</NuxtLink>
+  </div>
+</template>

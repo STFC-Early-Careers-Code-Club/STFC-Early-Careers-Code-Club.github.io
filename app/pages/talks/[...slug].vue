c3898174bfee85c6d3b609cea93219f8561233b7
George Roe <george.roe@stfc.ac.uk>
2026-01-09 09:47:49 +0000

    feat: improve styling of frontmatter display on talks pages

--- app/pages/talks/[...slug].vue
+++ app/pages/talks/[...slug].vue
@@ -11,16 +11,23 @@ if (!talk.value) {
 <template>
   <article class="w-full">
     <template v-if="talk">
-      <h1 class="mb-2">{{ talk.title }}</h1>
-      <p class="my-2 italic text-sm">{{ talk.speaker }} - {{ talk.date.toLocaleDateString() }}</p>
-      <img
-        v-if="talk.imgUrl"
-        :src="talk.imgUrl"
-        class="my-2 w-full h-48 rounded"
-        :class="`${talk.imgClass} ${talk.isImgLogo ? 'object-contain p-2' : 'object-cover'}`"
-      />
-      <p class="my-2">{{ talk.description }}</p>
-      <hr class="mt-2">
+      <div class="relative rounded-lg overflow-hidden mb-2">
+        <img
+          v-if="talk.imgUrl"
+          :src="talk.imgUrl"
+          class="absolute inset-0 m-0 w-full h-full blur-sm scale-110"
+          :class="`${talk.imgClass} ${talk.isImgLogo ? 'object-contain p-2' : 'object-cover'}`"
+        />
+        <div v-else class="bg-neutral-100 dark:bg-neutral-800 absolute inset-0 m-0 w-full h-full" />
+
+        <div class="p-2 relative" :class="{ 'bg-white/80 dark:bg-black/80': talk.imgUrl }">
+          <h1 class="mb-2">{{ talk.title }}</h1>
+          <p class="my-2 italic text-sm">
+            {{ talk.speaker }} - {{ talk.date.toLocaleDateString() }}
+          </p>
+          <p class="mt-2 mb-0">{{ talk.description }}</p>
+        </div>
+      </div>
       <ContentRenderer :value="talk" />
     </template>
   </article>

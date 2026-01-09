f7b79cff0c94d1f4dc9d3679c747d2712fcd6674
George Roe <george.roe@stfc.ac.uk>
2026-01-09 08:41:15 +0000

    feat: improve styling for talk search

--- app/pages/talks/index.vue
+++ app/pages/talks/index.vue
@@ -14,28 +14,25 @@ const talks = computed(() => {
 
 <template>
   <div class="w-full">
-    <h1>Talks</h1>
+    <h1 class="text-center">Talks</h1>
 
-    <p><NuxtLink to="/speaker-scoreboard">Speaker Scoreboard</NuxtLink></p>
-
-    <div class="flex gap-2 justify-center">
+    <div class="flex gap-2 justify-center mb-4">
       <Slider v-model="status" :options="[ 'upcoming', 'past' ]"></Slider>
       <Slider v-model="style" :options="[ 'cards', 'table' ]"></Slider>
-
     </div>
 
     <template v-if="talks && talks.length">
-      <h2>{{ status === 'upcoming' ? 'Upcoming Talks' : 'Past Talks' }}</h2>
       <TalkTable v-if="style === 'table'" :talks="talks" />
       <div v-else class="grid grid-cols-1 md:grid-cols-2 gap-2">
         <div v-for="talk in talks" :key="talk.id" class="bg-neutral-100 dark:bg-neutral-800 rounded-lg not-prose border-2 border-neutral-100 dark:border-neutral-800 text-center">
           <img
             :src="talk.imgUrl || '/images/no-image.jpg'"
+            :key="talk.path"
             :alt="`Image for ${talk.title}`"
             class="w-full h-48 rounded-md mb-2"
             :class="`${talk.imgClass} ${talk.imgUrl ? '' : 'dark:invert'} ${talk.isImgLogo ? 'object-contain p-2' : 'object-cover'}`"/>
           <NuxtLink :to="talk.path" class="hover:underline">
-            <h3 class="font-bold text-xl dark:text-white">{{ talk.title }}</h3>
+            <h2 class="font-bold text-xl dark:text-white">{{ talk.title }}</h2>
             <p class="text-sm">{{ talk.speaker }} - {{ new Date(talk.date).toLocaleDateString() }}</p>
           </NuxtLink>
           <hr class="my-1 mx-8" />

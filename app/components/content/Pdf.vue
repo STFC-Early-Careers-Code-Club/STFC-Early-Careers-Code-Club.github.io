d4f6e39be801588deb7fd29eeb8129a4939f566d
George Roe <george.roe@stfc.ac.uk>
2026-01-09 10:54:22 +0000

    feat: add pdf embed component

---
+++ app/components/content/Pdf.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  url: string
+}>()
+</script>
+
+<template>
+  <div>
+    <iframe
+      :src="url"
+      class="w-full h-full border-0"
+      allowfullscreen
+    ></iframe>
+    <NuxtLink :to="url">Open PDF in new tab</NuxtLink>
+  </div>
+</template>

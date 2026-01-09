72cca149bd52df0ccb0ac44c8c7c4deb632b728b
George Roe <george.roe@stfc.ac.uk>
2026-01-09 10:58:31 +0000

    feat: add github repo component

---
+++ app/components/content/GitHubRepo.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const props = defineProps<{ url: string }>()
+
+const match = props.url.match(/github\.com\/([^/]+)\/([^/]+)/)
+
+const owner = match?.[1]
+const repo = match?.[2]?.replace(/\.git$/, '')
+
+const ogImage =
+  owner && repo
+    ? `https://opengraph.githubassets.com/1/${owner}/${repo}`
+    : null
+</script>
+
+<template>
+  <NuxtLink
+    :to="url"
+    target="_blank"
+    rel="noopener"
+    class="underline decoration-dotted"
+  >
+    <img
+      v-if="ogImage"
+      :src="ogImage"
+      alt=""
+      class="not-prose block h-20 w-1/2"
+    />
+    <span>
+      Visit {{ owner }}/{{ repo }} on GitHub
+    </span>
+  </NuxtLink>
+</template>

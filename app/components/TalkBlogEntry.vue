33b737fcc2e2b5f941a069326762c56ac04373c2
George Roe <george.roe@stfc.ac.uk>
2026-03-02 14:00:15 +0000

    feat: show person avatar on talk blog entry

--- app/components/TalkBlogEntry.vue
+++ app/components/TalkBlogEntry.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import type { Talk } from '~/lib/sanitiseTalksCollectionItem';
 
-defineProps<{
+const props = defineProps<{
   talk: Talk
 }>()
+
+const person = usePerson(props.talk.speaker)
 </script>
 
 <template>
@@ -13,10 +15,16 @@ defineProps<{
     :date="talk.date"
     :image="talk.imgUrl || '/images/no-image.jpg'"
     :authors="[{
-      name: talk.speaker
+      name: talk.speaker,
+      avatar: person ? {
+        src: person.imgUrl,
+        icon: 'i-lucide-image'
+      } : undefined
     }]"
     :to="talk.path"
     :ui="{
+      title: 'break-words whitespace-normal line-clamp-none',
+      description: 'break-words whitespace-normal line-clamp-none',
       image: talk.imgUrl
         ? `${talk.imgClass} ${talk.isImgLogo ? 'object-contain p-2' : ''} object-center`
         : 'object-contain dark:invert bg-white'
---
+++ app/composables/usePerson.ts
@@ -0,0 +1,12 @@
+import type { Person } from "#imports";
+
+export function usePerson(name: string) {
+  const { data: person } = useAsyncData('person-' + name, async () => {
+    return queryCollection('people')
+      .where('name', '=', name)
+      .first()
+      .then(item => item ? item as Person : null)
+  })
+
+  return person
+}

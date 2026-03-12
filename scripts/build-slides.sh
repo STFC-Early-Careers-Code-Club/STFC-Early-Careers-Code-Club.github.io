f1f8bb96e37db9902ab3c5d2efc1d7a0250c4a1e
George Roe <george.roe@stfc.ac.uk>
2026-03-12 10:50:32 +0000

    feat: ensure build slides scripts resets dirs before running npm install

--- scripts/build-slides.sh
+++ scripts/build-slides.sh
@@ -18,6 +18,7 @@ for dir in "$SLIDEV_DIR"/*; do
 
     (
       cd "$dir"
+      rm -rf node_modules package-lock.json
       npm install
       npx @slidev/cli build --base "/slides/$TALK_NAME/" --out "../../$OUTPUT_DIR/$TALK_NAME"
     )
